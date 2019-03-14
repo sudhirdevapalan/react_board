8f4162f9d20ccbb5c0777ff9bb49460aacdefead
sudhir.godwin <sudhir.godwin@global-analytics.com>
2019-03-14 22:55:10 +0530

    removing unwanted files

--- react_app/src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import dashboardReducer from "./reducers/dashboardReducer";
-function configureStore(state = {}) {
-  return createStore(dashboardReducer,state);
-}
-export default configureStore;
