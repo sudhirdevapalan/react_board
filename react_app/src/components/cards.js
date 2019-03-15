49cbd99d04b9d19b74eb664aec7154a635908ac6
sudhir.godwin <sudhir.godwin@global-analytics.com>
2019-03-15 18:13:22 +0530

    removing unwanted files

--- react_app/src/components/cards.js
+++ react_app/src/components/cards.js
@@ -6,11 +6,11 @@ const CardComponent = (props) => (
 	<div draggable="true" className="wrp_card" id={props.cardName} onDragOver={(event)=>event.preventDefault()} onDragStart={(event)=>event.dataTransfer.setData("transfer",event.target.closest(".wrp_card").id+","+event.target.parentNode.closest(".wrp_list").id)}>
 		<h4>
 			{props.cardName}
-			<button className="btn_card_delete" type="button" onClick ={(elem)=>{props.deleteCard(elem.target.parentNode.closest(".wrp_card").id,elem.target.parentNode.closest(".wrp_list").id)}}>Delete Card</button>
+			<button className="btn btn-danger btn_card_delete" type="button" onClick ={(elem)=>{props.deleteCard(elem.target.parentNode.closest(".wrp_card").id,elem.target.parentNode.closest(".wrp_list").id)}}>Delete Card</button>
 		</h4>
 		<input type="text" placeholder="Card description" defaultValue={props.cardDesc} onChange = {(elem)=>{props.updateDesc(elem.target.value,elem.target.parentNode.closest(".wrp_card").id,elem.target.parentNode.closest(".wrp_list").id);}}/><br/>
 		<textarea id="xxx"></textarea><br/>
-		<button type="button" onClick={(elem)=>{props.addComment(elem.target.previousElementSibling.previousElementSibling.value,elem.target.parentNode.closest(".wrp_card").id,elem.target.parentNode.closest(".wrp_list").id);elem.target.previousElementSibling.previousElementSibling.value="";}}>Add Comment</button>
+		<button class="btn btn-warning" type="button" onClick={(elem)=>{props.addComment(elem.target.previousElementSibling.previousElementSibling.value,elem.target.parentNode.closest(".wrp_card").id,elem.target.parentNode.closest(".wrp_list").id);elem.target.previousElementSibling.previousElementSibling.value="";}}>Add Comment</button>
 		{props.cardComments.map(function(name, index){
 		return <div key={index} className="txt_comments">
 				{name}
--- react_app/src/components/lists.js
+++ react_app/src/components/lists.js
@@ -5,7 +5,7 @@ const ListComponent = (props) => (
 	<div draggable="true" className="wrp_list" onDragOver={(event)=>event.preventDefault()} id={props.listTitle} onDrop={(event)=>{props.drop(event.dataTransfer.getData("transfer"),event.target.closest(".wrp_list").id)}}>
 		<h2 className="txt_list_title">
 			{props.listTitle}
-			<button className="btn_list_delete" type="button" onClick = {(elem)=>{props.deleteList(elem.target.parentNode.closest(".wrp_list").id)}}>Delete List</button>
+			<button className=" btn btn-danger btn_list_delete" type="button" onClick = {(elem)=>{props.deleteList(elem.target.parentNode.closest(".wrp_list").id)}}>Delete List</button>
 		</h2>
 		{Object.keys(props.listData).map(function(name, index){
 			return <CardComponent key={index} updateDesc = {props.updateDesc} cardDesc = {props.listData[name]["desc"]} cardComments = {props.listData[name]["comments"]} deleteCard = {props.deleteCard} addComment = {props.addComment} cardName = {name}/>;
